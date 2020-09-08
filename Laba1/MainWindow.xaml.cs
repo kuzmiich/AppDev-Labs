b795cb00deb91cedb28be042c823025f77a66b62
иван кузьмич <ivankuzmic210@gmail.com>
2020-09-08 22:51:00 +0300

    Add handling try, catch

--- Laba1/MainWindow.xaml.cs
+++ Laba1/MainWindow.xaml.cs
@@ -25,31 +25,36 @@ namespace Laba1
         {
             InitializeComponent();
         }
+        private double ConvertToDouble(string value)
+        {
+            try
+            {
+                return Convert.ToDouble(value);
+            }
+            catch (System.FormatException ex)
+            {
+                Console.WriteLine("Error ", ex);
+                return 0;
+            }
+        }
         private double Calculate(double x, double y, double z)
         {
             return (Pow(y, (x + 1)) / (Pow(Abs(y - 2), 1 / 3) + 3)) + ((x + (y / 2)) / (2 * Abs(x + y)) * Pow(x + 1, -1 / Sin(z)));
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string Num1 = textBox1.Text;
-            string Num2 = textBox2.Text;
-            string Num3 = textBox3.Text;
-            double x = 0, y = 0, z = 0;
-            try
-            {
-                x = Convert.ToDouble(Num1);
-                y = Convert.ToDouble(Num2);
-                z = Convert.ToDouble(Num3);
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Error, ", ex);
-            }
+            string str1 = textBox1.Text;
+            string Str2 = textBox2.Text;
+            string Str3 = textBox3.Text;
+            double x = ConvertToDouble(str1);
+            double y = ConvertToDouble(str1);
+            double z = ConvertToDouble(str1);
+
             double result = Calculate(x, y, z);
             OutPanel.Text = "g = (y^(x+1) / (|x-y|^(1/3) + 3)) + ((x + y/2) / 2*|x+y|(x+1)^(-1/sin(z)))"+"\n"
-                + "X = " + Num1 + "\n"
-                + "Y = " + Num2 + "\n"
-                + "Z = " + Num3 + "\n"
+                + "X = " + str1 + "\n"
+                + "Y = " + Str2 + "\n"
+                + "Z = " + Str3 + "\n"
                 + "Результат = " + Round(result, 2);
         }
     }
