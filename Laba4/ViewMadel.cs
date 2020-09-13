715a12f3547e71317e5ae0247f2ed91e408b4ddb
иван кузьмич <ivankuzmic210@gmail.com>
2020-09-13 15:56:57 +0300

    .

--- Laba4/ViewMadel.cs
+++ Laba4/ViewMadel.cs
@@ -21,10 +21,25 @@ namespace Laba4
 
         public List<Grid> myElements { get; }
 
-        public void Main()
+        public void GridElement()
         {
             //DataGrid dataGrid = new DataGrid();
             //dataGrid.Arr = "1";
+        }
+        internal string StringSwap(int N, int M)
+        {
+            Random rand = new Random();
+            int[,] arr = new int[N, M];
+            int start = -100, end = 100;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    arr[i, j] = start + rand.Next(start, end);
+                    Console.WriteLine(arr[i, j]);
+                }
+            }
+            return "";
         }
     }
 }
